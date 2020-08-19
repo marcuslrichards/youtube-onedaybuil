58126d7319b4f70249f67ebab5f6c017f213e730
marcusrichards <marcusrichards@Naomis-MBP.mshome.net>
2020-08-19 07:40:01 +0100

    Initial Xcode Project Creation

---
+++ youtube-onedaybuil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuil
+//
+//  Created by marcusrichards on 19/08/2020.
+//  Copyright © 2020 marcusrichards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
