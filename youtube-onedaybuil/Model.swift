4931a4a7045b6baea1a8d705555aec2c67251e55
marcusrichards <marcusrichards@Naomis-MBP.mshome.net>
2020-08-19 09:06:31 +0100

    Additional Code Added

---
+++ youtube-onedaybuil/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  youtube-onedaybuil
+//
+//  Created by marcusrichards on 19/08/2020.
+//  Copyright © 2020 marcusrichards. All rights reserved.
+//
+
+import Foundation
---
+++ youtube-onedaybuil/Video.swift
@@ -0,0 +1,9 @@
+//
+//  Video.swift
+//  youtube-onedaybuil
+//
+//  Created by marcusrichards on 19/08/2020.
+//  Copyright © 2020 marcusrichards. All rights reserved.
+//
+
+import Foundation
